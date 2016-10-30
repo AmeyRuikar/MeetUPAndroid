6879df77d8e1b26e9fa2389d05196265fd6eb374
Amey Ruikar <ruikar@usc.edu>
2016-10-29 17:00:15 -0700

    login with fb(vedant's merge)

--- app/src/main/java/meetup/amey/com/meetup/loginActivity.java
+++ app/src/main/java/meetup/amey/com/meetup/loginActivity.java
@@ -16,6 +16,30 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.squareup.picasso.Picasso;
+import org.json.JSONException;
+import org.json.JSONObject;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.net.Uri;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import android.util.Base64;
+import java.util.*;
+
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 
@@ -31,6 +55,7 @@ public class loginActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_login);
         ButterKnife.inject(this);
 
@@ -47,6 +72,100 @@ public class loginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // Start the Signup activity
+
+                //facebook integration
+                try {
+                    PackageInfo info = getPackageManager().getPackageInfo(
+                            "com.example.vedant.facebooklogin",
+                            PackageManager.GET_SIGNATURES);
+                    for (Signature signature : info.signatures) {
+                        MessageDigest md = MessageDigest.getInstance("SHA");
+                        md.update(signature.toByteArray());
+                        Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+                    }
+                } catch (PackageManager.NameNotFoundException e) {
+
+                } catch (NoSuchAlgorithmException e) {
+
+                }
+
+
+
+                LoginButton loginButton;
+                CallbackManager callbackManager;
+
+
+                loginButton = (LoginButton) findViewById(R.id.login_button);
+                loginButton.setReadPermissions(Arrays.asList("public_profile", "email", "user_birthday", "user_friends"));
+                callbackManager = CallbackManager.Factory.create();
+
+
+                loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+
+                        //meetup.setVisibility(View.INVISIBLE);
+                        GraphRequest request = GraphRequest.newMeRequest(
+                                loginResult.getAccessToken(),
+                                new GraphRequest.GraphJSONObjectCallback() {
+                                    @Override
+                                    public void onCompleted(JSONObject object, GraphResponse response) {
+
+
+                                        try {
+                                            String email = object.getString("email");
+                                            String birthday = object.getString("birthday");
+                                            String id = object.getString("id");
+                                            String name = object.getString("name");
+                                           // tv_profile_name.setText(name);
+
+
+                                            String imageurl = "https://graph.facebook.com/" + id + "/picture?type=large";
+
+                                            //Picasso.with(MainActivity.this).load(imageurl).into(iv_profile_pic);
+                                            //iv_profile_pic.setVisibility(View.VISIBLE);
+                                        } catch (JSONException e) {
+                                            e.printStackTrace();
+                                        }
+
+                                    }
+                                });
+
+
+                        Bundle parameters = new Bundle();
+                        parameters.putString("fields", "id,name,email,gender, birthday");
+                        request.setParameters(parameters);
+                        request.executeAsync();
+
+
+                        /**
+                         * AccessTokenTracker to manage logout
+                         */
+                        AccessTokenTracker accessTokenTracker;
+                        accessTokenTracker = new AccessTokenTracker() {
+                            @Override
+                            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken,
+                                                                       AccessToken currentAccessToken) {
+                                if (currentAccessToken == null) {
+                                    //tv_profile_name.setText("");
+                                    //iv_profile_pic.setImageResource(R.drawable.maleicon);
+                                }
+                            }
+                        };
+                    }
+
+                    @Override
+                    public void onCancel() {
+
+                    }
+
+                    @Override
+                    public void onError(FacebookException error) {
+
+                    }
+                });
+
+                //eof FB integration
                 Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
                 startActivityForResult(intent, REQUEST_SIGNUP);
             }
